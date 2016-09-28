bd96335b83d765f94d491036f09e2b75633a3f8f
Luis Reyes <drdanielreyes5@gmail.com>
2016-09-27 20:57:59 -0600

    Agregandole Css, plantillas he implementado impervinculo de detalle a un titulo y que lo muestre en una pagina

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
-
+from blog.models import Articulo
+from django.utils import timezone
+from django.shortcuts import get_object_or_404
 # Create your views here.
 def articulo_lista(request):
-    return render(request, 'blog/articulo_lista.html', {})
+    articulos = Articulo.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('fecha_publicacion')
+    return render(request, 'blog/articulo_lista.html', {'articulos':articulos})
+
+def articulo_detalle(request, pk):
+    articulo = get_object_or_404(Articulo, pk=pk)
+    return render(request, 'blog/articulo_detalle.html', {'articulo': articulo})
